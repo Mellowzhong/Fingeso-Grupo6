84f4f9c699b79552178d9ef892dfdddce62d0eb2
Francisco Sánchez <franciscosanchez@Franciscos-MacBook-Air.local>
2024-08-04 20:51:19 -0400

    build:Init the backend and frontend

---
+++ BackendHabitatDigital/src/main/java/com/example/BackendHabitatDigital/BackendHabitatDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.BackendHabitatDigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendHabitatDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendHabitatDigitalApplication.class, args);
+	}
+
+}
