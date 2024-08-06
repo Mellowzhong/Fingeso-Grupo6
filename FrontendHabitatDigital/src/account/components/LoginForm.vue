ea20b0f715326e854e6a41c1a742c444cfc7cc11
Alonso Sanhueza <alonso.sanhueza32@gmail.com>
2024-08-05 20:26:45 -0400

    feat(frontend): implement tailwind and create common components
    - Implement tailwind for styling throughout the frontend.
    - Add icons for UI components.
    - Create header component with navigation.
    - Add account authentication modal for login and signup actions.
    - Start building the login and signup forms.

---
+++ FrontendHabitatDigital/src/account/components/LoginForm.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue';
+
+const email = ref('');
+const password = ref('');
+</script>
+
+<template>
+    <form action="" class="flex flex-col border-b-2 space-y-4 py-4">
+        <label for="">
+            <p class="text-sm ml-2 mb-1">Correo Electrónico:</p>
+            <div class="flex">
+                <input type="email" placeholder="Ingrese su correo" required class="w-full border rounded-l-lg pl-2 py-1">
+                <span class="material-symbols-rounded border rounded-r-lg p-1">mail</span>
+            </div>
+        </label>
+        <label for="">
+            <p class="text-sm ml-2 mb-1">Contraseña:</p>
+            <div class="flex">
+                <input type="password" placeholder="Ingrese su contraseña" required class="w-full border rounded-l-lg pl-2 py-1">
+                <span class="material-symbols-rounded border rounded-r-lg p-1">lock</span>
+            </div>
+        </label>
+        <button class="bg-teal-400 p-1 rounded">Iniciar sesion</button>
+    </form>
+</template>
