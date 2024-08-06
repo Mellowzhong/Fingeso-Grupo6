471ee93b387b87028155875481471fedd17a4e70
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-08-06 01:38:12 -0400

    feat(frontend): migrate the AccountInfo CSS native to TailwindCSS

--- FrontendHabitatDigital/src/App.vue
+++ FrontendHabitatDigital/src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import Header from './common/components/Header.vue';
+import AccountInfo from './account/views/AccountInfo.vue';
 </script>
 
 <template>
   <Header />
+  <AccountInfo />
 </template>
