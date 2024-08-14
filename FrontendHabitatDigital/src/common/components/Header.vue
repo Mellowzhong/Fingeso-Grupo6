975532357833f354c97a100a3abefb83b7e75b9e
Francisco Sanchez <francisco.sanchez.g@usach.cl>
2024-08-14 15:22:35 -0400

    feat(front):add vuex and vuex-persist

--- FrontendHabitatDigital/src/common/components/Header.vue
+++ FrontendHabitatDigital/src/common/components/Header.vue
@@ -1,40 +1,35 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import AccountAuthModal from './AccountAuthModal.vue';
-import router from '../../Router/Router';
+import { useStore } from 'vuex';
 
 const isAccountAuthModalOpen = ref(false);
+const store = useStore();
+const usuario = computed(() => store.getters.getUsuario);
 
 const handleIsAccountAuthModalOpen = () => {
     isAccountAuthModalOpen.value = !isAccountAuthModalOpen.value;
 }
 
-const redirect = () => {
-    router.push({ name: 'UserInfo' });
-}
 </script>
 
 <template>
     <header>
-        <nav class="bg-blue-950 text-teal-400 flex justify-between px-32 py-2 space-x-32">
+        <nav class="bg-blue-950 text-teal-400 flex justify-between px-32 py-2">
             <div class="flex items-center space-x-4">
                 <button class="material-symbols-rounded">menu</button>
                 <h1>Logo</h1>
+                <form action="" class="flex w-96 pl-10">
+                    <input type=" text" placeholder="Buscar un inmueble" class="w-full px-3 py-2 rounded-l-xl">
+                    <button class="bg-teal-400 text-blue-950 grid px-3 py-2 rounded-r-xl">
+                        <span class="material-symbols-rounded">search</span>
+                    </button>
+                </form>
             </div>
-            <form action="" class="flex w-full">
-                <input type="text" placeholder="Buscar un inmueble" class="w-full px-3 py-2 rounded-l-xl">
-                <button class="bg-teal-400 text-blue-950 grid px-3 py-2 rounded-r-xl">
-                    <span class="material-symbols-rounded">search</span>
-                </button>
-            </form>
             <div class="flex items-center space-x-4">
                 <button class="material-symbols-rounded">notifications</button>
                 <button @click="handleIsAccountAuthModalOpen" class="material-symbols-rounded">account_circle</button>
-            </div>
-            <div>
-                <button @click.prevent="redirect()">
-                    user
-                </button>
+                <span v-if="!usuario.firstName"> {{ usuario.firstName }} </span>
             </div>
         </nav>
         <AccountAuthModal v-model:isOpen="isAccountAuthModalOpen" v-model:onClose="handleIsAccountAuthModalOpen" />
--- FrontendHabitatDigital/src/main.js
+++ FrontendHabitatDigital/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import router from "./Router/Router.js";
+import store from "./Store/Store.js";
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(store).mount("#app");
